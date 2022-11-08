4c6c62087c53f422f80aeae1269bd00fada725b3
Daniel Dimitrov <daniel@compojoom.com>
2022-11-08 14:17:51 +0100

    Add circleCi deploy to docker hub

--- scripts/deploy_docker.sh
+++ scripts/deploy_docker.sh
@@ -4,13 +4,13 @@ set -euo pipefail
 
 if [ "$1" = "develop" -o "$1" = "master" ]; then
     # If image does not exist, don't use cache
-    docker pull safeglobal/$DOCKERHUB_PROJECT:$1 && \
-    docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile . --cache-from safeglobal/$DOCKERHUB_PROJECT:$1 || \
+    docker pull trustlines/$DOCKERHUB_PROJECT:$1 && \
+    docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile . --cache-from trustlines/$DOCKERHUB_PROJECT:$1 || \
     docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile .
 else
-    docker pull safeglobal/$DOCKERHUB_PROJECT:staging && \
-    docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile . --cache-from safeglobal/$DOCKERHUB_PROJECT:staging || \
+    docker pull trustlines/$DOCKERHUB_PROJECT:staging && \
+    docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile . --cache-from trustlines/$DOCKERHUB_PROJECT:staging || \
     docker build -t $DOCKERHUB_PROJECT -f docker/web/Dockerfile .
 fi
-docker tag $DOCKERHUB_PROJECT safeglobal/$DOCKERHUB_PROJECT:$1
-docker push safeglobal/$DOCKERHUB_PROJECT:$1
+docker tag $DOCKERHUB_PROJECT trustlines/$DOCKERHUB_PROJECT:$1
+docker push trustlines/$DOCKERHUB_PROJECT:$1
